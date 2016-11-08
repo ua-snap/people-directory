03e1d766d2445eb920981af4c0792e4cf6f070bc
Craig Stephenson <crstephenson@alaska.edu>
2016-11-07 17:41:34 -0900

    Add website field to Person template.

--- themes/directory/templates/node/node--person.tpl.php
+++ themes/directory/templates/node/node--person.tpl.php
@@ -166,6 +166,7 @@
         <?php endif; ?>
         <?php print render($content['field_expertise']); ?>
         <?php print render($content['group_publications']); ?>
+        <?php print render($content['field_website']); ?>
         <?php if (!empty($node->field_edir_name)): ?>
           <?php $edir_biography = $node->field_edir_name['und'][0]['entity']->field_biography; ?>
           <?php if (!empty($edir_biography)): ?>
